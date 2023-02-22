f4242176e2e2e0b655f40d07348ba8eeb8d1a444
James Miller <jam1211miller@gmail.com>
2023-02-22 09:29:49 -0700

    pdf conversion

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -9,28 +9,26 @@ const ImageSlider = () => {
   const carousel = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setWidth(carousel.current!.scrollWidth - carousel.current!.offsetWidth);
+    setWidth(carousel.current!.clientWidth - carousel.current!.scrollWidth);
   }, []);
 
   return (
-    <div>
-      <motion.div ref={carousel} className="cursor-grab overflow-hidden">
-        <motion.div drag="x" dragConstraints={{ right: 0, left: -width }}>
-          <motion.div className="pointer-events-none flex space-x-4 ">
-            {caroselImages.map((image, index) => (
-              <motion.div key={index}>
-                <Image
-                  src={image}
-                  alt={"Why you should hire me"}
-                  width={"700"}
-                  className="min-w-[11rem]"
-                ></Image>
-              </motion.div>
-            ))}
-          </motion.div>
+    <motion.div ref={carousel} className="cursor-grab overflow-hidden">
+      <motion.div drag="x" dragConstraints={{ right: 0, left: width }}>
+        <motion.div className="pointer-events-none flex space-x-4 ">
+          {caroselImages.map((image, index) => (
+            <motion.div key={index}>
+              <Image
+                src={image}
+                alt={"Why you should hire me"}
+                width={"700"}
+                className="min-w-[11rem]"
+              ></Image>
+            </motion.div>
+          ))}
         </motion.div>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
